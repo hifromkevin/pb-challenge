1d88b403366ffa46dfa328173befd0549956d4a5
Kevin Lam <klam@Kevins-Macbook-Pro.local>
2019-07-24 09:57:14 -0700

    Renders image, price, and name to screen

--- js/render-products.js
+++ js/render-products.js
@@ -7,4 +7,23 @@ fetch('./products.json')
 		let jsonData = JSON.stringify(data);
 		localStorage.setItem('products', jsonData);
 		console.log('ls', localStorage);
-	})
+
+		data.groups.forEach(item => {
+			// Since pricing is inconsistent among list items, determine how price is listed
+			if (item.priceRange) {
+				price = item.priceRange.regular ? 
+								item.priceRange.regular.high : 
+								item.priceRange.selling.high;
+			} else {
+				price = item.price.regular;
+			}
+
+			list += `
+			<div class="box ${item.id}" id="${item.id}">
+				<img class="box__thumb" src="${item.thumbnail.href}" alt="${item.name}" />
+				<span class="box__title">${item.name}</span>
+				<span class="box__price">$${price}</span>
+			</div>`;
+			});
+			if (list.length) document.getElementById('productGrid').innerHTML = list;
+		})
